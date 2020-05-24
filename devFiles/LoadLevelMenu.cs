80f7e1492127c2a81f0cc8a4b6f1989fe01c71ce
Matthew Paul <mpaul0416@gmail.com>
2020-05-24 12:34:36 -0400

    Reskin LoadLevelMenu

--- devFiles/LoadLevelMenu.cs
+++ devFiles/LoadLevelMenu.cs
@@ -1,4 +1,5 @@
 using Godot;
+using RacingThoughts.misc;
 using System;
 
 public class LoadLevelMenu : CanvasLayer
@@ -9,10 +10,17 @@ public class LoadLevelMenu : CanvasLayer
 	{
 		_loader = GetParent<LevelLoader>();
 		_levelSeedText = GetNode<TextEdit>("Panel/MarginContainer/VBoxContainer/TextEdit");
+
+		_levelSeedText.Text = RandomSingleton.GetInstance().NextSeed();
 	}
 
 	public void OnLoadLevelButtonPressed()
 	{
 		_loader.LoadLevel("res://worlds/World.tscn", _levelSeedText.Text);
+	}
+
+	public void OnMainMenuButtonPressed()
+	{
+		_loader.LoadMenu("res://MainMenu.tscn");
 	}
 }
